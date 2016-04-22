e395cb5f5ce0edafcdb694610270a602aee18f33
Amanda Cameron <amanda@darkdna.net>
2016-04-22 14:37:03 -0400

    Add material design iconography that I missed in the last commit.

---
+++ icons/README.md
@@ -0,0 +1,3 @@
+Theses are a few simple icons for use with the go-search library.
+
+They are downloaded and transformed from the [Google Material Design Iconography](https://design.google.com/icons/).
---
+++ icons/icons.go
@@ -0,0 +1,5 @@
+package icons
+
+//go:generate embed-result-image -package icons info.png Info
+//go:generate embed-result-image -package icons error.png Error
+//go:generate embed-result-image -package icons warning.png Warning
